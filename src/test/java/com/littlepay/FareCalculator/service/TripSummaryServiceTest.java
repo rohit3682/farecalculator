c14de3577fcbd1ba462973b6c8884b8e48c7ff76
Rohit Agarwal <rohit.agarwal@iboa.com.au>
2024-11-29 23:13:00 +1100

    Adding more TripSummaryService tests.

--- src/test/java/com/littlepay/FareCalculator/service/TripSummaryServiceTest.java
+++ src/test/java/com/littlepay/FareCalculator/service/TripSummaryServiceTest.java
@@ -15,6 +15,9 @@ import java.util.Map;
 public class TripSummaryServiceTest {
 
     private static List<BusTripSummary> busTripSummaryList;
+    private static List<BusTripSummary> completedTrips;
+    private static List<BusTripSummary> cancelledTrips;
+    private static List<BusTripSummary> incompleteTrips;
 
     @BeforeAll
     public static void setUp() throws IOException {
@@ -24,6 +27,15 @@ public class TripSummaryServiceTest {
         fareService.loadFareData();
         Map<String, List<Trip>> tripData = csvUtil.readTapsData("taps.csv");
         busTripSummaryList = tripSummaryService.generateTripSummary(tripData);
+        completedTrips = busTripSummaryList.stream()
+                .filter(trip -> Constants.TRIP_COMPLETE.equalsIgnoreCase(trip.getStatus()))
+                .toList();
+        cancelledTrips = busTripSummaryList.stream()
+                .filter(trip -> Constants.TRIP_CANCELLED.equalsIgnoreCase(trip.getStatus()))
+                .toList();
+        incompleteTrips = busTripSummaryList.stream()
+                .filter(trip -> Constants.TRIP_INCOMPLETE.equalsIgnoreCase(trip.getStatus()))
+                .toList();
     }
 
     @Test
@@ -33,37 +45,21 @@ public class TripSummaryServiceTest {
 
     @Test
     public void testCompleteTripsCount() {
-        List<BusTripSummary> completedTrips = busTripSummaryList.stream()
-                .filter(trip -> Constants.TRIP_COMPLETE.equalsIgnoreCase(trip.getStatus()))
-                .toList();
-
         Assertions.assertEquals(10, completedTrips.size());
     }
 
     @Test
     public void testCancelledTripsCount() {
-        List<BusTripSummary> cancelledTrips = busTripSummaryList.stream()
-                .filter(trip -> Constants.TRIP_CANCELLED.equalsIgnoreCase(trip.getStatus()))
-                .toList();
-
         Assertions.assertEquals(2, cancelledTrips.size());
     }
 
     @Test
     public void testIncompleteTripsCount() {
-        List<BusTripSummary> incompleteTrips = busTripSummaryList.stream()
-                .filter(trip -> Constants.TRIP_INCOMPLETE.equalsIgnoreCase(trip.getStatus()))
-                .toList();
-
         Assertions.assertEquals(3, incompleteTrips.size());
     }
 
     @Test
     public void testIncompleteTrips() {
-        List<BusTripSummary> incompleteTrips = busTripSummaryList.stream()
-                .filter(trip -> Constants.TRIP_INCOMPLETE.equalsIgnoreCase(trip.getStatus()))
-                .toList();
-
         List<BusTripSummary> tempList = incompleteTrips.stream()
                 .filter(trip -> "1400001555555558".equalsIgnoreCase(trip.getPan()))
                 .toList();
@@ -85,10 +81,6 @@ public class TripSummaryServiceTest {
 
     @Test
     public void testCancelledTrips() {
-        List<BusTripSummary> cancelledTrips = busTripSummaryList.stream()
-                .filter(trip -> Constants.TRIP_CANCELLED.equalsIgnoreCase(trip.getStatus()))
-                .toList();
-
         List<BusTripSummary> tempList = cancelledTrips.stream()
                 .filter(trip -> "4111111111111111".equalsIgnoreCase(trip.getPan()))
                 .toList();
@@ -104,10 +96,6 @@ public class TripSummaryServiceTest {
 
     @Test
     public void testCompleteTrips() {
-        List<BusTripSummary> completedTrips = busTripSummaryList.stream()
-                .filter(trip -> Constants.TRIP_COMPLETE.equalsIgnoreCase(trip.getStatus()))
-                .toList();
-
         List<BusTripSummary> tempList = completedTrips.stream()
                 .filter(trip -> "5500005555555559".equalsIgnoreCase(trip.getPan()))
                 .toList();
@@ -164,8 +152,90 @@ public class TripSummaryServiceTest {
     }
 
     @Test
-    public void testFromStopToStop() {
+    public void testCompletedTripStopIds() {
+        List<BusTripSummary> tempList = completedTrips.stream()
+                .filter(trip -> "5500005555555559".equalsIgnoreCase(trip.getPan()))
+                .toList();
 
+        Assertions.assertEquals("Stop1", tempList.get(0).getFromStopId());
+        Assertions.assertEquals("Stop2", tempList.get(0).getToStopId());
+
+        Assertions.assertEquals("Stop2", tempList.get(1).getFromStopId());
+        Assertions.assertEquals("Stop3", tempList.get(1).getToStopId());
+    }
+
+    @Test
+    public void testCompletedTripBusIds() {
+        List<BusTripSummary> tempList = completedTrips.stream()
+                .filter(trip -> "1200001333333336".equalsIgnoreCase(trip.getPan()))
+                .toList();
+
+        Assertions.assertEquals("Bus44", tempList.get(0).getBusId());
+    }
+
+    @Test
+    public void testCompletedTripsFare() {
+        List<BusTripSummary> tempList = completedTrips.stream()
+                .filter(trip -> "9000009999999993".equalsIgnoreCase(trip.getPan()))
+                .toList();
+
+        Assertions.assertEquals(1.93, tempList.get(0).getChargeAmt());
+    }
+
+    @Test
+    public void testIncompletedTripStopIds() {
+        List<BusTripSummary> tempList = incompleteTrips.stream()
+                .filter(trip -> "4111111111111111".equalsIgnoreCase(trip.getPan()))
+                .toList();
+
+        Assertions.assertNull(tempList.get(0).getToStopId());
+        Assertions.assertEquals("Stop3", tempList.get(0).getFromStopId());
+    }
+
+    @Test
+    public void testIncompletedTripBusIds() {
+        List<BusTripSummary> tempList = incompleteTrips.stream()
+                .filter(trip -> "7000007777777771".equalsIgnoreCase(trip.getPan()))
+                .toList();
+
+        Assertions.assertEquals("Bus39", tempList.get(0).getBusId());
+    }
+
+    @Test
+    public void testIncompletedTripsFare() {
+        List<BusTripSummary> tempList = incompleteTrips.stream()
+                .filter(trip -> "1400001555555558".equalsIgnoreCase(trip.getPan()))
+                .toList();
+
+        Assertions.assertEquals(9.54, tempList.get(0).getChargeAmt());
+    }
+
+    @Test
+    public void testCancelledTripStopIds() {
+        List<BusTripSummary> tempList = cancelledTrips.stream()
+                .filter(trip -> "6000006666666660".equalsIgnoreCase(trip.getPan()))
+                .toList();
+
+        Assertions.assertEquals("Stop7", tempList.get(0).getFromStopId());
+        Assertions.assertEquals("Stop7", tempList.get(0).getToStopId());
+    }
+
+    @Test
+    public void testCancelledTripBusIds() {
+        List<BusTripSummary> tempList = cancelledTrips.stream()
+                .filter(trip -> "6000006666666660".equalsIgnoreCase(trip.getPan()))
+                .toList();
+
+        Assertions.assertEquals("Bus38", tempList.get(0).getBusId());
+    }
+
+    @Test
+    public void testCancelledTripsFare() {
+        List<BusTripSummary> tempList = cancelledTrips.stream()
+                .filter(trip -> "4111111111111111".equalsIgnoreCase(trip.getPan()))
+                .toList();
+
+        Assertions.assertEquals(0, tempList.get(0).getChargeAmt());
     }
 
 }
